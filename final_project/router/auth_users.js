046c7b0523342a0570ecb147bbdd7226b2b02c48
Sameer Shaikh <60113690+sameer550@users.noreply.github.com>
2023-03-19 16:12:06 +0000

    Update auth_users.js

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -7,6 +7,7 @@ let users = [];
 
 const isValid = (username)=>{ //returns boolean
 //write code to check is the username is valid
+// git push checking
 }
 
 const authenticatedUser = (username,password)=>{ //returns boolean
